19d0d44eba8dc062b0ca354471c5a1ab745604c2
MohamedMorabet <medmrbt5@gmail.com>
2024-11-15 12:39:55 +0100

    🚧 get_next_line: On the path to victory – fixing leaks one at a time! 💦

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mel-mora <mel-mora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 09:31:17 by mel-mora          #+#    #+#             */
-/*   Updated: 2024/11/15 10:41:43 by mel-mora         ###   ########.fr       */
+/*   Updated: 2024/11/15 12:36:55 by mel-mora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,21 +29,28 @@ int	didnt_reach_end(char *p)
 char	*get_next_line(int fd)
 {
 	char		*s;
+	char		*tmp;
 	ssize_t		bytes_read;
 	static char	p[BUFFER_SIZE + 1];
 
 	s = NULL;
 	if (fd > 0)
 	{
-		read(fd, p, BUFFER_SIZE);
-		while (didnt_reach_end(p) == 1)
+		while (1)
 		{
-			s = ft_strjoin(s, p);
 			bytes_read = read(fd, p, BUFFER_SIZE);
 			if (bytes_read <= 0)
 				break ;
+			p[bytes_read] = '\0';
+			if (didnt_reach_end(p) != 1)
+				break;
+			tmp = ft_strjoin(s, p);
+			if (s)
+				free (s);
+			s = tmp;
 		}
 		s = ft_strjoin(s, ft_substr(p, 0, bytes_read));
+		free (tmp);
 	}
 	return (s);
 }
